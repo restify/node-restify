289773c88cd881b3c39a52b810cb636ad70bdabf
Simon Sturmer <sstur@me.com>
2012-06-01 20:37:04 +1000

    add request body length validation to form_body_parser; add test

--- test/server.test.js
+++ test/server.test.js
@@ -1036,3 +1036,46 @@ test('GH-141 return next(err) not working', function (t) {
 
 // });
 //
+
+
+test('GH-149 limit request body size', function (t) {
+  var server = restify.createServer();
+  server.use(restify.bodyParser({maxBodySize: 1024}));
+
+  server.post('/', function (req, res, next) {
+    res.send(200, {length: req.body.length});
+    return next();
+  });
+
+  server.listen(PORT, function () {
+    var opts = {
+      hostname: 'localhost',
+      port: PORT,
+      path: '/',
+      method: 'POST',
+      agent: false,
+      headers: {
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded',
+        'transfer-encoding': 'chunked'
+      }
+    };
+    var req = http.request(opts, function (res) {
+      t.equal(res.statusCode, 413);
+      var body = '';
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
+      res.on('end', function () {
+        //throw new Error(body);
+        //t.equal(body, 'invalid credentials');
+        server.close(function () {
+          t.end();
+        });
+      });
+    });
+    req.write(new Array(1028).join('x'));
+    req.end();
+  });
+});
