092e03b12cd9a7426a19769150ad6082c33b4dc0
Mark Cavage <mark.cavage@joyent.com>
2012-12-27 01:34:45 +0000

    #195 JSONP support

---
+++ examples/jsonp/jsonp.js
@@ -0,0 +1,13 @@
+var restify = require('../../lib');
+
+var srv = restify.createServer();
+srv.use(restify.queryParser());
+srv.use(restify.jsonp());
+srv.get('/', function (req, res, next) {
+        res.send({hello: 'world'});
+        next();
+});
+
+srv.listen(8080, function () {
+        console.log('ready on %s', srv.url);
+});
