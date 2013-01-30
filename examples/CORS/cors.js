31dd8a19e53e8b31471b582b3ed30211f05d0a5e
Mark Cavage <mark.cavage@joyent.com>
2013-01-30 18:13:17 +0000

    #284 example

---
+++ examples/CORS/cors.js
@@ -0,0 +1,16 @@
+var restify = require('./lib');
+
+var srv = restify.createServer();
+srv.use(restify.CORS());
+
+function foo(req, res, next) {
+        res.send(204);
+        next();
+}
+
+srv.put('/foo', foo);
+srv.get('/foo', foo);
+srv.del('/foo', foo);
+srv.post('/foo', foo);
+
+srv.listen(8080);
