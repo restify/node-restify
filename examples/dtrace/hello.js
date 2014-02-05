bcdbda4b97d6d4621b502c3bc1dc36e10c70fee8
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-02-05 09:44:44 +0100

    Convert 8 spaces indent to 4 - update linter

--- examples/dtrace/hello.js
+++ examples/dtrace/hello.js
@@ -1,7 +1,7 @@
 var restify = require('../lib');
 
 var server = restify.createServer({
-        name: 'helloworld'
+    name: 'helloworld'
 });
 
 server.use(restify.acceptParser(server.acceptable));
@@ -11,21 +11,21 @@ server.use(restify.queryParser());
 server.use(restify.urlEncodedBodyParser());
 
 server.use(function slowHandler(req, res, next) {
-        setTimeout(function() {
-                next();
-        }, 250);
+    setTimeout(function () {
+        next();
+    }, 250);
 });
 
 server.get({
-        path: '/hello/:name',
-        name: 'GetFoo'
+    path: '/hello/:name',
+    name: 'GetFoo'
 }, function respond(req, res, next) {
-        res.send({
-                hello: req.params.name
-        });
-        next();
+    res.send({
+        hello: req.params.name
+    });
+    next();
 });
 
-server.listen(8080, function() {
-        console.log('listening: %s', server.url);
+server.listen(8080, function () {
+    console.log('listening: %s', server.url);
 });
--- examples/sockio/sockio.js
+++ examples/sockio/sockio.js
@@ -6,28 +6,26 @@ var socketio = require('socket.io');
 var restify = require('../lib');
 
 
-
 ///--- Globals
 
 var IP = (function () {
-        var nics = os.networkInterfaces();
-        var keys = Object.keys(nics).filter(function (k) {
-                return (nics[k].some(function (n) {
-                        return (!n.internal);
-                }));
-        });
-        return (nics[keys.pop()].pop().address);
+    var nics = os.networkInterfaces();
+    var keys = Object.keys(nics).filter(function (k) {
+        return (nics[k].some(function (n) {
+            return (!n.internal);
+        }));
+    });
+    return (nics[keys.pop()].pop().address);
 })();
 
 var HTML = '<script src="/socket.io/socket.io.js"></script>\n' +
-        '<script>\n' +
-          'var socket = io.connect("http://' + IP + ':8080");\n' +
-          'socket.on("news", function (data) {\n' +
-            'console.log(data);\n' +
-            'socket.emit("my other event", { my: "data" });\n' +
-        '});\n' +
-        '</script>';
-
+    '<script>\n' +
+    'var socket = io.connect("http://' + IP + ':8080");\n' +
+    'socket.on("news", function (data) {\n' +
+    'console.log(data);\n' +
+    'socket.emit("my other event", { my: "data" });\n' +
+    '});\n' +
+    '</script>';
 
 
 ///--- Mainline
@@ -36,22 +34,22 @@ var server = restify.createServer();
 var io = socketio.listen(server);
 
 server.get('/', function indexHTML(req, res, next) {
-        res.setHeader('Content-Type', 'text/html');
-        res.setHeader('Content-Length', Buffer.byteLength(HTML));
-        res.writeHead(200);
-        res.write(HTML);
-        res.end();
-        next();
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(HTML));
+    res.writeHead(200);
+    res.write(HTML);
+    res.end();
+    next();
 });
 
 
 io.sockets.on('connection', function (socket) {
-        socket.emit('news', { hello: 'world' });
-        socket.on('my other event', function (data) {
-                console.log(data);
-        });
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
 
 server.listen(8080, IP, function () {
-        console.log('socket.io server listening at %s', server.url);
+    console.log('socket.io server listening at %s', server.url);
 });
