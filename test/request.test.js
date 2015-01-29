8692f37d90f59198a3abdfd1af523763eafa8ff3
jerome creignou <jerome.creignou@gmail.com>
2015-01-29 19:33:38 +0100

    add test on request to reproduce bug

---
+++ test/request.test.js
@@ -0,0 +1,17 @@
+// Copyright 2012 Mark Cavage, Inc.  All rights reserved.
+var http = require('http');
+require('../lib/request');
+
+var Request = http.IncomingMessage;
+
+module.exports = {
+    'request is upload': function (test) {
+        var methods = {'PATCH' : true, 'POST' : true, 'PUT': true, 'GET': false, 'HEAD' : false, 'DELETE' : false, 'TRACE':  false}
+        Object.keys.forEach(function (m) {
+            test.equal(Request.prototype.isUpload.apply({
+                method: m
+            }), methods[m], 'upload status for ' + m + ' is not valid');
+        });
+        test.done();
+    }
+};
