457a0d27aeed163f49a6653ffa258aa4177dd5c9
Armin Tamzarian <tamzarian1989@gmail.com>
2015-02-16 11:29:28 -0600

    Fix #751 incorrect server.url when using IPv6

--- test/server.test.js
+++ test/server.test.js
@@ -98,6 +98,20 @@ test('listen and close (socketPath)', function (t) {
 });
 
 
+test('gh-751 IPv4/IPv6 server URL', function (t) {
+    t.equal(SERVER.url, 'http://127.0.0.1:' + PORT, 'ipv4 url');
+
+    var server = restify.createServer();
+    server.listen(PORT + 1, '::1', function () {
+        t.equal(server.url, 'http://[::1]:' + (PORT + 1), 'ipv6 url');
+
+        server.close(function () {
+            t.end();
+        });
+    });
+});
+
+
 test('get (path only)', function (t) {
     var r = SERVER.get('/foo/:id', function echoId(req, res, next) {
         t.ok(req.params);
