0c68c94dfcf8202c658519c6965ebf323da22a44
Michal Moskal <michal.moskal@microsoft.com>
2015-04-14 16:37:42 -0700

    Add missing semicolons

--- test/plugins.test.js
+++ test/plugins.test.js
@@ -975,15 +975,15 @@ test('GH-774 utf8 corruption in body parser', function (t) {
     SERVER.post('/utf8',
         restify.bodyParser({ mapParams: false }),
         function (req, res, next) {
-            t.notOk(/\ufffd/.test(req.body.text))
-            t.equal(req.body.text.length, slen)
+            t.notOk(/\ufffd/.test(req.body.text));
+            t.equal(req.body.text.length, slen);
             res.send({ len: req.body.text.length });
             next();
         });
 
     // create a long string of unicode characters
-    var tx = ""
-    for (var i = 0; i < slen; ++i) tx += "\u2661"
+    var tx = "";
+    for (var i = 0; i < slen; ++i) tx += "\u2661";
 
     CLIENT.post('/utf8', { text: tx }, function (err, _, res) {
         t.ifError(err);
