5aa44a965e0d147275e76735307e8a06cd472d3c
Sopl'Wang <sopl.wang@gmail.com>
2015-04-22 00:29:01 +0800

    Add unit test for #791 and #792

--- test/plugins.test.js
+++ test/plugins.test.js
@@ -400,6 +400,33 @@ test('body json ok (no params)', function (t) {
     });
 });
 
+test('body json ok (large)', function (t) {
+    SERVER.post('/body/:id',
+        restify.bodyParser({ mapParams: false }),
+        function (req, res, next) {
+            t.equal(req.params.id, 'foo');
+            t.equal(req.params.name, 'markc');
+            t.notOk(req.params.phone);
+            t.equal(req.body.phone, '(206) 555-1212');
+            res.send();
+            next();
+        });
+
+    var obj = {
+        phone: '(206) 555-1212',
+        name: 'somethingelse',
+        extra: []
+    };
+    for (var i = 0; i<10000; i++) {
+        obj.extra.push('padding padding');
+    }
+    CLIENT.post('/body/foo?name=markc', obj, function (err, _, res) {
+        t.ifError(err);
+        t.equal(res.statusCode, 200);
+        t.end();
+    });
+});
+
 test('body json ok (null params)', function (t) {
     var STRING_CLIENT = restify.createStringClient({
         url: 'http://127.0.0.1:' + PORT,
