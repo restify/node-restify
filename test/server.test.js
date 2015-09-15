27fb47e39bb8e45506724e24d0cdc7096c20ecec
Alex Liu <donutespresso@gmail.com>
2015-09-14 18:06:06 -0700

    fire all error events, always. return any errors from emitters to client

--- test/server.test.js
+++ test/server.test.js
@@ -2047,7 +2047,8 @@ test('GH-733 if request closed early, stop processing. ensure only ' +
 
 test('GH-667 emit error event for generic Errors', function (t) {
 
-    var count = 0;
+    var restifyError = 0;
+    var notFoundFired = false;
 
     SERVER.get('/1', function (req, res, next) {
         return next(new Error('foobar'));
@@ -2059,25 +2060,53 @@ test('GH-667 emit error event for generic Errors', function (t) {
 
     SERVER.get('/3', function (req, res, next) {
         SERVER.on('NotFound', function (req2, res2, err, cb) {
+            notFoundFired = true;
             t.ok(err);
             t.equal(err instanceof errors.NotFoundError, true);
-            t.equal(count, 2);
+            t.equal(restifyError, 2);
             t.end();
             return cb();
         });
         return next(new errors.NotFoundError('foobar'));
     });
 
-    SERVER.on('error', function (req, res, err, cb) {
-        count++;
+    SERVER.on('restifyError', function (req, res, err, cb) {
+        restifyError++;
         t.ok(err);
         t.equal(err instanceof Error, true);
         return cb();
     });
 
-    function noop() {}
-    CLIENT.get('/1', noop);
-    CLIENT.get('/2', noop);
-    CLIENT.get('/3', noop);
+    CLIENT.get('/1', function (err, req, res, data) {
+        // should get regular error
+        t.ok(err);
+    });
+    CLIENT.get('/2', function (err, req, res, data) {
+        // should get not found error
+        t.ok(err);
+    });
+    CLIENT.get('/3', function (err, req, res, data) {
+        // should get notfounderror
+        t.ok(err);
+        t.equal(notFoundFired, true);
+    });
+});
+
+
+test('GH-667 error handler to return another error', function (t) {
+
+    SERVER.on('ImATeapot', function (req, res, err, cb) {
+        return cb(new errors.LockedError('oh noes'));
+    });
+
+    SERVER.get('/1', function (req, res, next) {
+        return next(new errors.ImATeapotError('foobar'));
+    });
+
+    CLIENT.get('/1', function (err, req, res, data) {
+        t.ok(err);
+        t.equal(err.name, 'LockedError');
+        t.end();
+    });
 });
 
