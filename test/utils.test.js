aef0a3da1c20b5e29a005e9bb2b949814cb1b29c
retrohacker <william.jblankenship@gmail.com>
2017-05-15 12:13:00 -0700

    Create unit tests for sanitizePath

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var mergeQs = require('../lib/utils').mergeQs;
+var utils = require('../lib/utils.js');
+var mergeQs = utils.mergeQs;
+var sanitizePath = utils.sanitizePath;
 
 if (require.cache[__dirname + '/lib/helper.js']) {
     delete require.cache[__dirname + '/lib/helper.js'];
@@ -27,6 +29,33 @@ test('merge qs', function (t) {
 
     var qs5 = mergeQs(null, null);
     t.deepEqual(qs5, {});
+
+    t.done();
+});
+
+test('sanitizePath', function (t) {
+    // Ensure it santizies potential edge cases correctly
+    var tests = {
+        input: [
+            '////foo////', //excess padding on both ends
+            'bar/foo/', // trailing slash
+            'bar/foo/////', // multiple trailing slashes
+            'foo////bar', // multiple slashes inbetween
+            '////foo', // multiple at beginning
+            '/foo/bar' // don't mutate
+        ],
+        output: [
+            '/foo',
+            'bar/foo',
+            'foo/bar',
+            '/foo',
+            '/foo/bar'
+        ]
+    };
+
+    for (var i = 0; i < tests; i++) {
+        t.equal(sanitizePath(tests.input[i]), tests.output[i]);
+    }
 
     t.done();
 });
