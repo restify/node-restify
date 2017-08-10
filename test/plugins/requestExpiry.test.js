44735315dae56a6bb40689e8eb27d4e52efa5eb4
Yunong Xiao <yjxiao@gmail.com>
2017-08-10 12:25:27 -0700

    CR fixes, add additional tests

--- test/plugins/requestExpiry.test.js
+++ test/plugins/requestExpiry.test.js
@@ -41,6 +41,40 @@ describe('request expiry parser', function () {
     });
 
 
+    describe('constructor', function () {
+
+        it('should throw if no headers passed in', function (done) {
+            try {
+                SERVER.use(restify.plugins.requestExpiry({}));
+            } catch (e) {
+                done();
+            }
+        });
+
+
+        it('should throw if only timeout header passed in', function (done) {
+            try {
+                SERVER.use(restify.plugins.requestExpiry({
+                    timeoutHeader: 'foo'
+                }));
+            } catch (e) {
+                done();
+            }
+        });
+
+
+        it('should throw if only timeout header passed in', function (done) {
+            try {
+                SERVER.use(restify.plugins.requestExpiry({
+                    startHeader: 'foo'
+                }));
+            } catch (e) {
+                done();
+            }
+        });
+    });
+
+
     describe('absolute header', function () {
 
         it('should timeout due to request expiry', function (done) {
@@ -116,7 +150,40 @@ describe('request expiry parser', function () {
 
             var obj = {
                 path: getPath,
-                headers: { }
+                headers: {
+                    'x-request-expiry': Date.now() + 100
+                }
+            };
+
+            CLIENT.get(obj, function (err, _, res) {
+                assert.equal(res.statusCode, 200);
+                assert.equal(called, true);
+                assert.ifError(err);
+                done();
+            });
+        });
+
+
+        it('should be ok if request expiry header is NaN', function (done) {
+            var key = 'x-request-expiry';
+            var getPath = '/request/expiry';
+            var called = false;
+
+            SERVER.use(restify.plugins.requestExpiry({ absoluteHeader: key }));
+            SERVER.get(getPath, function (req, res, next) {
+                // requests never expire if the header is not set
+                assert.isFalse(req.isExpired());
+                called = true;
+                res.send();
+                next();
+            });
+
+            var obj = {
+                path: getPath,
+                headers: {
+                    'x-request-expiry':
+                    'I am just a poor boy with my story seldom told'
+                }
             };
 
             CLIENT.get(obj, function (err, _, res) {
@@ -219,6 +286,111 @@ describe('request expiry parser', function () {
             var obj = {
                 path: getPath,
                 headers: { }
+            };
+
+            CLIENT.get(obj, function (err, _, res) {
+                assert.equal(res.statusCode, 200);
+                assert.equal(called, true);
+                assert.ifError(err);
+                done();
+            });
+        });
+
+
+        it('should be ok if start header is NaN', function (done) {
+            var startKey = 'x-request-starttime';
+            var timeoutKey = 'x-request-timeout';
+            var getPath = '/request/expiry';
+            var called = false;
+
+            SERVER.use(restify.plugins.requestExpiry({
+                startHeader: startKey,
+                timeoutHeader: timeoutKey
+            }));
+            SERVER.get(getPath, function (req, res, next) {
+                // requests never expire if the header is not set
+                assert.isFalse(req.isExpired());
+                called = true;
+                res.send();
+                next();
+            });
+
+            var obj = {
+                path: getPath,
+                headers: {
+                    'x-request-starttime': 'I have squandered my resistance',
+                    'x-request-timeout': 100
+                }
+            };
+
+            CLIENT.get(obj, function (err, _, res) {
+                assert.equal(res.statusCode, 200);
+                assert.equal(called, true);
+                assert.ifError(err);
+                done();
+            });
+        });
+
+
+        it('should be ok if timeout header is NaN', function (done) {
+            var startKey = 'x-request-starttime';
+            var timeoutKey = 'x-request-timeout';
+            var getPath = '/request/expiry';
+            var called = false;
+
+            SERVER.use(restify.plugins.requestExpiry({
+                startHeader: startKey,
+                timeoutHeader: timeoutKey
+            }));
+            SERVER.get(getPath, function (req, res, next) {
+                // requests never expire if the header is not set
+                assert.isFalse(req.isExpired());
+                called = true;
+                res.send();
+                next();
+            });
+
+            var obj = {
+                path: getPath,
+                headers: {
+                    'x-request-starttime': Date.now(),
+                    'x-request-timeout': 'For a pocked full of mumbles'
+                }
+            };
+
+            CLIENT.get(obj, function (err, _, res) {
+                assert.equal(res.statusCode, 200);
+                assert.equal(called, true);
+                assert.ifError(err);
+                done();
+            });
+        });
+
+
+        it('should be ok if both headers are NaN', function (done) {
+            var startKey = 'x-request-starttime';
+            var timeoutKey = 'x-request-timeout';
+            var getPath = '/request/expiry';
+            var called = false;
+
+            SERVER.use(restify.plugins.requestExpiry({
+                startHeader: startKey,
+                timeoutHeader: timeoutKey
+            }));
+            SERVER.get(getPath, function (req, res, next) {
+                // requests never expire if the header is not set
+                assert.isFalse(req.isExpired());
+                called = true;
+                res.send();
+                next();
+            });
+
+            var obj = {
+                path: getPath,
+                headers: {
+                    'x-request-starttime': 'Such are promises',
+                    'x-request-timeout': 'All lies and jests'
+                }
             };
 
             CLIENT.get(obj, function (err, _, res) {
