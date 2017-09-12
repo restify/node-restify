9a1040aa5166232dd3a5c5acd46e6f7fe11ab0f0
Peter Marton <email@martonpeter.com>
2017-09-12 17:59:24 +0200

    chore(examples): fix sockio example (#1487)

--- examples/sockio/sockio.js
+++ examples/sockio/sockio.js
@@ -1,26 +1,15 @@
 // Copyright 2012 Mark Cavage, Inc.  All rights reserved.
 
-var os = require('os');
 var socketio = require('socket.io');
 
-var restify = require('../lib');
+var restify = require('../../lib');
 
 
 ///--- Globals
 
-var IP = (function () {
-    var nics = os.networkInterfaces();
-    var keys = Object.keys(nics).filter(function (k) {
-        return (nics[k].some(function (n) {
-            return (!n.internal);
-        }));
-    });
-    return (nics[keys.pop()].pop().address);
-})();
-
 var HTML = '<script src="/socket.io/socket.io.js"></script>\n' +
     '<script>\n' +
-    'var socket = io.connect("http://' + IP + ':8080");\n' +
+    'var socket = io("http://localhost:8080");\n' +
     'socket.on("news", function (data) {\n' +
     'console.log(data);\n' +
     'socket.emit("my other event", { my: "data" });\n' +
@@ -31,7 +20,7 @@ var HTML = '<script src="/socket.io/socket.io.js"></script>\n' +
 ///--- Mainline
 
 var server = restify.createServer();
-var io = socketio.listen(server);
+var io = socketio(server.server);
 
 server.get('/', function indexHTML(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
@@ -43,13 +32,13 @@ server.get('/', function indexHTML(req, res, next) {
 });
 
 
-io.sockets.on('connection', function (socket) {
+io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
     });
 });
 
-server.listen(8080, IP, function () {
+server.listen(8080, function () {
     console.log('socket.io server listening at %s', server.url);
 });
