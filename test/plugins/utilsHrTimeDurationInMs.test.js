f9224c92f85130703547c6e4ef47c6e788229d86
Peter Marton <peter@risingstack.com>
2017-09-30 13:37:10 +0200

    fix(plugins): use process.hrtime() for duration calculation

---
+++ test/plugins/utilsHrTimeDurationInMs.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var assert = require('chai').assert;
+var hrTimeDurationInMs =
+    require('../../lib/plugins/utils/hrTimeDurationInMs');
+
+describe('utils #hrTimeDurationInMs', function () {
+
+    it('should return with duration', function () {
+        var startTime = [0, 0];
+        var endTime = [1, 1e6];
+
+        var duration = hrTimeDurationInMs(startTime, endTime);
+
+        assert.equal(duration, 1001);
+    });
+
+});
