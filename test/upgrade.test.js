22604e463fc55c7ea1cceaf4d91244c5d137f104
Alex Liu <donutespresso@gmail.com>
2019-04-12 15:25:37 -0700

    properly fix test

--- test/upgrade.test.js
+++ test/upgrade.test.js
@@ -160,7 +160,8 @@ test('GET without upgrade headers', function(t) {
 
 test('Dueling upgrade and response handling 1', function(t) {
     var done = finish_latch(t, {
-        'expected requestUpgrade error': 1
+        'expected requestUpgrade error': 1,
+        'client response': 1
     });
 
     SERVER.get('/attach', function(req, res, next) {
@@ -173,9 +174,7 @@ test('Dueling upgrade and response handling 1', function(t) {
         }
 
         try {
-            var upg = res.claimUpgrade();
-            // TODO we never reach this destroy() call. do we still need it?
-            upg.socket.destroy();
+            res.claimUpgrade();
         } catch (ex) {
             done('expected requestUpgrade error');
         }
@@ -202,6 +201,12 @@ test('Dueling upgrade and response handling 1', function(t) {
                 t.ifError(err2);
             }
             t.equal(res.statusCode, 400);
+            res.on('end', function() {
+                done('client response');
+            });
+            // noop data listener required for resume to take effect
+            res.on('data', function() {});
+            res.resume();
         });
         req.on('upgradeResult', function(err2, res) {
             done('server upgraded unexpectedly');
