23a80aeb9321b3fcaa4aebf2f35ff3118ae515bb
ghermeto <gui.hermeto@gmail.com>
2022-05-07 20:42:46 -0700

    chore(examples): update todoapp

--- examples/todoapp/test/todo.test.js
+++ examples/todoapp/test/todo.test.js
@@ -4,127 +4,130 @@ var fs = require('fs');
 
 var pino = require('pino');
 var restify = require('restify');
+var assert = require('chai').assert;
 
 var todo = require('../lib');
 
 ///--- Globals
 
-var CLIENT;
 var DIR = '/tmp/.todo_unit_test';
-var SERVER;
 var SOCK = '/tmp/.todo_sock';
 
 ///--- Tests
 
-exports.setup = function(t) {
-    var log = pino({
-        name: 'todo_unit_test',
-        level: process.env.LOG_LEVEL || 'info'
-    });
+describe('todoapp', function () {
+    var CLIENT;
+    var SERVER;
 
-    fs.mkdir(DIR, function(err) {
-        if (err && err.code !== 'EEXIST') {
-            console.error('unable to mkdir: ' + err.stack);
-            process.exit(1);
-        }
-
-        SERVER = todo.createServer({
-            directory: DIR,
-            log: log.child({ component: 'server' }, true),
-            noAudit: true
+    before(function (done) {
+        var log = pino({
+            name: 'todo_unit_test',
+            level: process.env.LOG_LEVEL || 'info'
         });
-
-        t.ok(SERVER);
-        SERVER.listen(SOCK, function() {
-            CLIENT = todo.createClient({
-                log: log.child({ component: 'client' }, true),
-                socketPath: SOCK
+    
+        fs.mkdir(DIR, function(err) {
+            if (err && err.code !== 'EEXIST') {
+                console.error('unable to mkdir: ' + err.stack);
+                process.exit(1);
+            }
+    
+            SERVER = todo.createServer({
+                directory: DIR,
+                log: log.child({ component: 'server' }, true),
+                noAudit: true
             });
-            t.ok(CLIENT);
-            t.done();
+    
+            assert.ok(SERVER);
+            SERVER.listen(SOCK, function() {
+                CLIENT = todo.createClient({
+                    log: log.child({ component: 'client' }, true),
+                    socketPath: SOCK
+                });
+                assert.ok(CLIENT);
+                done();
+            });
+        });    
+    });
+
+    it('should return an empty list', function (done) {
+        CLIENT.list(function(err, todos) {
+            assert.ifError(err);
+            assert.ok(todos);
+            assert.ok(Array.isArray(todos));
+    
+            if (todos) {
+                assert.equal(todos.length, 0);
+            }
+            done();
         });
     });
-};
 
-exports.listEmpty = function(t) {
-    CLIENT.list(function(err, todos) {
-        t.ifError(err);
-        t.ok(todos);
-        t.ok(Array.isArray(todos));
-
-        if (todos) {
-            t.equal(todos.length, 0);
-        }
-        t.done();
+    it('should create a new task', function (done) {
+        var task = 'check that unit test works';
+        CLIENT.create(task, function(err, todo) {
+            assert.ifError(err);
+            assert.ok(todo);
+    
+            if (todo) {
+                assert.ok(todo.name);
+                assert.equal(todo.task, task);
+            }
+            done();
+        });
     });
-};
 
-exports.create = function(t) {
-    var task = 'check that unit test works';
-    CLIENT.create(task, function(err, todo) {
-        t.ifError(err);
-        t.ok(todo);
-
-        if (todo) {
-            t.ok(todo.name);
-            t.equal(todo.task, task);
-        }
-        t.done();
+    it('should list and get', function (done) {
+        CLIENT.list(function(err, todos) {
+            assert.ifError(err);
+            assert.ok(todos);
+            assert.ok(Array.isArray(todos));
+    
+            if (todos) {
+                assert.equal(todos.length, 1);
+                CLIENT.get(todos[0], function(err2, todo) {
+                    assert.ifError(err2);
+                    assert.ok(todo);
+                    done();
+                });
+            } else {
+                done();
+            }
+        });
     });
-};
 
-exports.listAndGet = function(t) {
-    CLIENT.list(function(err, todos) {
-        t.ifError(err);
-        t.ok(todos);
-        t.ok(Array.isArray(todos));
-
-        if (todos) {
-            t.equal(todos.length, 1);
-            CLIENT.get(todos[0], function(err2, todo) {
-                t.ifError(err2);
-                t.ok(todo);
-                t.done();
-            });
-        } else {
-            t.done();
-        }
+    it('should update', function (done) {
+        CLIENT.list(function(err, todos) {
+            assert.ifError(err);
+            assert.ok(todos);
+            assert.ok(Array.isArray(todos));
+    
+            if (todos) {
+                assert.equal(todos.length, 1);
+    
+                var todo = {
+                    name: todos[0],
+                    task: 'something else'
+                };
+                CLIENT.update(todo, function(err2) {
+                    assert.ifError(err2);
+                    done();
+                });
+            } else {
+                done();
+            }
+        });
     });
-};
 
-exports.update = function(t) {
-    CLIENT.list(function(err, todos) {
-        t.ifError(err);
-        t.ok(todos);
-        t.ok(Array.isArray(todos));
-
-        if (todos) {
-            t.equal(todos.length, 1);
-
-            var todo = {
-                name: todos[0],
-                task: 'something else'
-            };
-            CLIENT.update(todo, function(err2) {
-                t.ifError(err2);
-                t.done();
-            });
-        } else {
-            t.done();
-        }
-    });
-};
-
-exports.teardown = function teardown(t) {
-    CLIENT.del(function(err) {
-        t.ifError(err);
-
-        SERVER.once('close', function() {
-            fs.rmdir(DIR, function(err) {
-                t.ifError(err);
-                t.done();
+    after(function(done) {
+        CLIENT.del(function(err) {
+            assert.ifError(err);
+            CLIENT.client.close();
+            SERVER.close(function () {
+                fs.rmdir(DIR, function(err) {
+                    assert.ifError(err);
+                    done();
+                });
             });
         });
-        SERVER.close();
     });
-};
+});
