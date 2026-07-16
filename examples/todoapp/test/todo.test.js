f6e5bbff22079a586758957a2d704c43dcb0a3b2
Dziyana Vinakur <55085195+dianager@users.noreply.github.com>
2026-07-16 12:01:57 +0200

    feat: replace module.parent with explicit module param in test helper (#2001)
    Co-authored-by: dvinakur <dvinakur@netflix.com>

--- examples/todoapp/test/todo.test.js
+++ examples/todoapp/test/todo.test.js
@@ -15,28 +15,28 @@ var SOCK = '/tmp/.todo_sock';
 
 ///--- Tests
 
-describe('todoapp', function () {
+describe('todoapp', function() {
     var CLIENT;
     var SERVER;
 
-    before(function (done) {
+    before(function(done) {
         var log = pino({
             name: 'todo_unit_test',
             level: process.env.LOG_LEVEL || 'info'
         });
-    
+
         fs.mkdir(DIR, function(err) {
             if (err && err.code !== 'EEXIST') {
                 console.error('unable to mkdir: ' + err.stack);
                 process.exit(1);
             }
-    
+
             SERVER = todo.createServer({
                 directory: DIR,
                 log: log.child({ component: 'server' }, true),
                 noAudit: true
             });
-    
+
             assert.ok(SERVER);
             SERVER.listen(SOCK, function() {
                 CLIENT = todo.createClient({
@@ -46,15 +46,15 @@ describe('todoapp', function () {
                 assert.ok(CLIENT);
                 done();
             });
-        });    
+        });
     });
 
-    it('should return an empty list', function (done) {
+    it('should return an empty list', function(done) {
         CLIENT.list(function(err, todos) {
             assert.ifError(err);
             assert.ok(todos);
             assert.ok(Array.isArray(todos));
-    
+
             if (todos) {
                 assert.equal(todos.length, 0);
             }
@@ -62,12 +62,12 @@ describe('todoapp', function () {
         });
     });
 
-    it('should create a new task', function (done) {
+    it('should create a new task', function(done) {
         var task = 'check that unit test works';
         CLIENT.create(task, function(err, todo) {
             assert.ifError(err);
             assert.ok(todo);
-    
+
             if (todo) {
                 assert.ok(todo.name);
                 assert.equal(todo.task, task);
@@ -76,12 +76,12 @@ describe('todoapp', function () {
         });
     });
 
-    it('should list and get', function (done) {
+    it('should list and get', function(done) {
         CLIENT.list(function(err, todos) {
             assert.ifError(err);
             assert.ok(todos);
             assert.ok(Array.isArray(todos));
-    
+
             if (todos) {
                 assert.equal(todos.length, 1);
                 CLIENT.get(todos[0], function(err2, todo) {
@@ -95,15 +95,15 @@ describe('todoapp', function () {
         });
     });
 
-    it('should update', function (done) {
+    it('should update', function(done) {
         CLIENT.list(function(err, todos) {
             assert.ifError(err);
             assert.ok(todos);
             assert.ok(Array.isArray(todos));
-    
+
             if (todos) {
                 assert.equal(todos.length, 1);
-    
+
                 var todo = {
                     name: todos[0],
                     task: 'something else'
@@ -122,7 +122,7 @@ describe('todoapp', function () {
         CLIENT.del(function(err) {
             assert.ifError(err);
             CLIENT.client.close();
-            SERVER.close(function () {
+            SERVER.close(function() {
                 fs.rmdir(DIR, function(err) {
                     assert.ifError(err);
                     done();
